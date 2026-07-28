684849ba8ac16bfee1d930413c3042c5ad91bf5c
Bhalani Harshit <hbhalani937@gmail.com>
2026-07-28 13:57:31 +0530

    Time: 0 ms (100%), Space: 9 MB (35.24%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        
+        int count=0;
+        for (int i = s.length()-1; i >=0 ; i--)
+        {
+
+            if (s[i]!=' ')
+            {
+                count++;
+            }
+            
+            if (s[i]==' ' && count>0)
+            {
+                break;
+            }
+            
+            
+        }
+
+        return count;
+        
+    }
+};
